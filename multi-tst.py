7d5ebba9be3f30e3a215a4cc1e1231ba06b0357d
Ron Coe Jr <roncoejr@hotmail.com>
2024-04-27 22:48:27 -0400

    working use cases both cli and streamlit

--- multi-tst.py
+++ multi-tst.py
@@ -2,36 +2,63 @@ import speech_recognition as sr
 from langdetect import detect_langs
 import pandas as pd
 import sys
+from dotenv import load_dotenv
+import azure.cognitiveservices.speech as speechsdk
 
-from os import path, listdir
+from os import path, listdir, environ
 
-
+load_dotenv()
 AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), sys.argv[1])
 
-pd.set_option('display.max_colwidth', None)
-r = sr.Recognizer()
-data = {'the_transcript': [''], 'the_languages': ['']}
-df = pd.DataFrame(data)
-n_recordings = 0
-for filename in listdir(sys.argv[1]):
-    n_recordings += 1
-    if filename.endswith(".wav") or filename.endswith(".aif"):
-        with sr.AudioFile(path.join(sys.argv[1], filename)) as source:
-            audio = r.record(source)
+az_api_key = environ['azureapikey']
+az_region = environ['azureregion']
+
+def az_text_extract(the_audio, api_key: str) -> str:
+    # configure Azure cognitive speech services
+    speech_config = speechsdk.SpeechConfig(subscription = az_api_key, region = az_region)
+    speech_config.speech_recognition_language = "en-US"
+    speech_config.endpoint_silence_timeout_ms = 25000
+
+    audio_config = speechsdk.audio.AudioConfig(filename=the_audio)
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+    # call the Azure speech transcription service
+    speech_recognition_result = speech_recognizer.recognize_once_async().get()
+
+    the_text_transcription = speech_recognition_result.text
+
+    # return the transcribed text
+    return the_text_transcription
+
+def main(the_path: str):
+    pd.set_option('display.max_colwidth', None)
+    r = sr.Recognizer()
+    data = {'the_transcript': [''], 'the_languages': ['']}
+    data_all = {'sph_the_transcript': [''], 'sph_the_languages': [''], 'az_the_transcript': [''], 'az_the_languages': ['']}
+    df_sph = pd.DataFrame(data)
+    df_az = pd.DataFrame(data)
+    df_all = pd.DataFrame(data_all)
+    n_recordings = 0
+    for filename in listdir(the_path):
+        n_recordings += 1
+        if filename.endswith(".wav"):
+            with sr.AudioFile(path.join(the_path, filename)) as source:
+                audio = r.record(source)
+            try:
+                recognized_text_sphinx = r.recognize_sphinx(audio)
+                recognized_text_azure = az_text_extract(path.join(the_path, filename), az_api_key)
+                df_sph.loc[len(df_sph)] = [recognized_text_sphinx, detect_langs(recognized_text_sphinx)]
+                df_az.loc[len(df_az)] = [recognized_text_azure, detect_langs(recognized_text_azure)]
+            except sr.UnknownValueError:
+                print("Audio not understood")
+            except sr.RequestError as e:
+                print("Error {}".format(e))
+
+        df_all.loc[len(df_all)] = [df_sph['the_transcript'], df_sph['the_languages'], df_az['the_transcript'], df_az['the_languages']]
+
+    for i in range(1, len(df_all)):
+        print("\n{} |--| {} : {} >----< {}\n".format(df_all.loc[i]['sph_the_transcript'], df_all.loc[i]['sph_the_languages'], df_all.loc[i]['az_the_transcript'], df_all.loc[i]['az_the_languages']))
+    df_all.info()
 
 
-        try:
-            recognized_text = r.recognize_sphinx(audio)
-            df.loc[len(df)] = [recognized_text, detect_langs(recognized_text)]
-            print("\n\nRecording {} processed\n".format(n_recordings))
-        except sr.UnknownValueError:
-            print("Audio not understood")
-        except sr.RequestError as e:
-            print("Error {}".format(e))
-
-
-# print("\n\n\nThe transcript reads: {}\n\nLanguage(s) Detected: {}\n\n".format(recognized_text, detect_langs(recognized_text)))
-# print("{}\n".format(df.head()))
-for i in range(1, len(df)):
-    print("\n{} |--| {}\n".format(df.loc[i]['the_transcript'], df.loc[i]['the_languages']))
-df.info()
+if __name__ == "__main__":
+    main(sys.argv[1])
--- st_tst.py
+++ st_tst.py
@@ -4,33 +4,73 @@ import pandas as pd
 import sys
 import streamlit as st
 from io import StringIO, BytesIO
+import azure.cognitiveservices.speech as speechsdk
+from dotenv import load_dotenv
 
-from os import path, listdir
+from os import path, listdir, environ
 
-
+load_dotenv()
 AUDIO_FILES = st.file_uploader("Select files", ['wav'], accept_multiple_files=True)
 
-pd.set_option('display.max_colwidth', None)
-r = sr.Recognizer()
-data = {'the_transcript': [''], 'the_languages': ['']}
-df = pd.DataFrame(data)
-n_recordings = 0
-if AUDIO_FILES is not None:
-    for filename in AUDIO_FILES:
-        n_recordings += 1
-        d_bytes = BytesIO(filename.getvalue())
-        with sr.AudioFile(d_bytes) as source:
-            audio = r.record(source)
+az_api_key = environ['azureapikey']
+az_region = environ['azureregion']
+
+SCRIPT_PATH = path.dirname(path.abspath(__file__))
+
+def az_text_extract(the_audio, api_key: str) -> str:
+    # configure Azure cognitive speech services
+    speech_config = speechsdk.SpeechConfig(subscription = az_api_key, region = az_region)
+    speech_config.speech_recognition_language = "en-US"
+    speech_config.endpoint_silence_timeout_ms = 25000
+
+    audio_config = speechsdk.audio.AudioConfig(filename=the_audio)
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+    # call the Azure speech transcription service
+    speech_recognition_result = speech_recognizer.recognize_once_async().get()
+
+    the_text_transcription = speech_recognition_result.text
+
+    # return the transcribed text
+    return the_text_transcription
+
+def write_temp_file(the_file):
+    with open(path.join(SCRIPT_PATH, the_file.name), "wb") as tmp_file:
+        tmp_file.write(the_file.getbuffer())
 
 
-        try:
-            recognized_text = r.recognize_sphinx(audio)
-            df.loc[len(df)] = [recognized_text, detect_langs(recognized_text)]
-            st.write("\n\nRecording {} processed\n".format(n_recordings))
-        except sr.UnknownValueError:
-            st.write("Audio not understood")
-        except sr.RequestError as e:
-            st.write("Error {}".format(e))
+def main():
+    pd.set_option('display.max_colwidth', None)
+    r = sr.Recognizer()
+    data = {'the_transcript': [''], 'the_languages': ['']}
+    data_all = {'sph_the_transcript': [''], 'sph_the_languages': [''], 'az_the_transcript': [''], 'az_the_languages': ['']}
+    df_sph = pd.DataFrame(data)
+    df_az = pd.DataFrame(data)
+    df_all = pd.DataFrame(data_all)
+    n_recordings = 0
+    if AUDIO_FILES is not None:
+        for filename in AUDIO_FILES:
+            n_recordings += 1
+            d_bytes = BytesIO(filename.getvalue())
+            with sr.AudioFile(d_bytes) as source:
+                audio = r.record(source)
+            try:
+                recognized_text_sphinx = r.recognize_sphinx(audio)
+                write_temp_file(filename)
+                recognized_text_azure = az_text_extract(path.join(SCRIPT_PATH, filename.name), az_api_key)
+                df_sph.loc[len(df_sph.index)] = [recognized_text_sphinx, detect_langs(recognized_text_sphinx)]
+                df_az.loc[len(df_az.index)] = [recognized_text_azure, detect_langs(recognized_text_azure)]
+                st.write("\n\nRecording {} processed\n".format(n_recordings))
+            except sr.UnknownValueError:
+                st.write("Audio not understood")
+            except sr.RequestError as e:
+                st.write("Error {}".format(e))
 
-df
-df.info()
+            df_all.loc[len(df_all.index)] = [df_sph['the_transcript'][n_recordings], df_sph['the_languages'][n_recordings], df_az['the_transcript'][n_recordings], df_az['the_languages'][n_recordings]]
+
+    df_all
+    df_all.info()
+
+
+
+if __name__ == "__main__":
+    main()
